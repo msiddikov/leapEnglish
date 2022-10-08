2eedb5ba9c045cbd1d84dc7be8d5cc61a3234072
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-09 00:41:54 +0500

    addition field added

--- models/userModel.js
+++ models/userModel.js
@@ -44,6 +44,7 @@ const User = sequelize.define(
       },
     },
     access: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+    addition: { type: DataTypes.STRING },
   },
   {
     indexes: [
