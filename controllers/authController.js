eaaf8705321fc231b5b133281ceaf26e9a7a9d4e
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-08 12:20:06 +0500

    user auth finishs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,23 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const createJwt = (id) => {
+  return jwt.sign({ id }, "leapengish_good", {
+    expiresIn: "30d",
+  });
+};
 
 exports.signup = async (req, res, next) => {
   try {
     const { full_name, email, password } = req.body;
+    const user = await User.create({ full_name, email, password });
+    const accessToken = createJwt(user.id);
+    res.status(200).json({
+      isOk: true,
+      data: { user, accessToken },
+      message: "Successfull signup",
+    });
   } catch (error) {
     res.status(404).json({
       isOk: false,
@@ -14,5 +29,25 @@ exports.signup = async (req, res, next) => {
 
 exports.signin = async (req, res, next) => {
   try {
-  } catch (error) {}
+    const { email, password } = req.body;
+    if (!email || !password)
+      throw new Error("Siz email va password kiritmadingiz");
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) throw new Error("Wrong Email or password");
+    const check = bcrypt.compare(password, user.password);
+    if (!check) throw new Error("Wrong Email or password");
+
+    res.status(200).json({
+      isOk: true,
+      data: { user, accessToken },
+      message: "Successfull login",
+    });
+  } catch (error) {
+    res.status(404).json({
+      isOk: false,
+      message: error.message,
+      data: "",
+    });
+  }
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const sequelize = require("../configs/db");
 const { DataTypes } = require("sequelize");
+const bcrypt = require("bcryptjs");
 
 const User = sequelize.define("users", {
   id: {
@@ -12,4 +13,8 @@ const User = sequelize.define("users", {
   password: { type: DataTypes.STRING, allowNull: false },
 });
 
+User.beforeCreate(async (user, options) => {
+  const hashedPassword = await bcrypt.hash(user.password, 12);
+  user.password = hashedPassword;
+});
 module.exports = User;
