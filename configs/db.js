f367c8e204877b37208702e75fcdbf1cea7c1084
Mason Siddikov <m.siddikov@gmail.com>
2022-11-26 15:22:48 +0500

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    dialect: "mysql",
+    dialect: "postgresql",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
   }
