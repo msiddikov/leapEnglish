58b953cf625e88dff51989207e06b1f8b968fd3f
Pulatoff <niyozbekpulatov@gmail.com>
2023-01-25 18:15:11 +0500

    add token

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,13 +3,13 @@ const axios = require("axios");
 exports.getVideo = async (req, res, next) => {
   try {
     const { videoId } = req.params;
-    const { ttl } = req.body;
+    const { ttl, token } = req.body;
     const response = await axios.post(
       `https://dev.vdocipher.com/api/videos/${videoId}/otp`,
-      { ttl },
+      { ttl, token },
       {
         headers: {
-          Authorization: `Apisecret kHhUHc9PDE1lutYrg540YSQDYKONc2exVqeC9LLno4dKRbZrd0zS6t7VAvdl3aMm`,
+          Authorization: `Apisecret ${token}`,
         },
       }
     );
@@ -17,7 +17,7 @@ exports.getVideo = async (req, res, next) => {
     res.status(200).json({
       data: response.data,
       isOk: true,
-      message: "you success get video optio  ns",
+      message: "you success get video options",
     });
   } catch (error) {
     res.status(404).json({ data: "", isOk: false, message: error.message });
